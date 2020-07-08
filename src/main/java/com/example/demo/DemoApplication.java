fcf29d5804c85d028bb39dc7cfcef12f8e7864df
veritablevein <veritablevein@gmail.com>
2020-07-08 16:12:06 +0800

    add information of author

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,6 +13,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("change one's mind");
 	}
 
 }
