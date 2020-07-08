75e8f02ec323b7a2bedf52c63334284ff19c01c6
veritablevein <veritablevein@gmail.com>
2020-07-08 16:02:43 +0800

    add information of author

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+@author
+王浩淼
+ */
+
 @SpringBootApplication
 public class DemoApplication {
 
